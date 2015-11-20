2b15d9ff403e1b71130efa0383c356554fd1ab50
Edward Dumholt <edumholt@gmail.com>
2015-11-19 20:21:18 -0600

    added random diamonds

--- js/platformerGame.js
+++ js/platformerGame.js
@@ -2,6 +2,8 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create:
 
 var platforms,
     player,
+    baddie,
+    diamonds,
     score = 0,
     numStars = 12,
     playerLives = 3,
@@ -14,6 +16,7 @@ function preload() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
+    game.load.image('diamond', 'assets/diamond.png')
     game.load.audio('collectStar', 'assets/collect_star.mp3');
     game.load.audio('explode', 'assets/explode.mp3');
     game.load.audio('ugh', 'assets/ugh.mp3');
@@ -64,17 +67,15 @@ function create() {
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
-    // Define two animations for our dude
-    // walking left and walking right
+    // Define two animations for our dude walking left and walking right
     //
-    // They should run at 10 frames per second
-    // and they should loop.
+    // They should run at 10 frames per second and they should loop.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    // Create baddie
     launchBaddie();
 
-    //
     // Drop a sprinkling of stars into the scene and allow
     // the player to collect them.
     //
@@ -97,6 +98,10 @@ function create() {
 
     }
 
+    // Add diamonds group
+    diamonds = game.add.group();
+    diamonds.enableBody = true;
+
     // Add audio
     collectStarSound = game.add.audio('collectStar');
     deadSound = game.add.audio('explode');
@@ -111,11 +116,9 @@ function create() {
                                                                   fill: '#000'});
 
     // Set up our game controls
-    // Phaser has a builtin keyboard manager and one of its benefits
-    // is the createCursorKeys method
-    // This populates the cursors boject with four properties
-    // up, down, left, right. These properties are instances
-    // of Phaser.Key
+    // Phaser has a builtin keyboard manager and one of its benefits is the createCursorKeys method
+    // This populates the cursors boject with four properties: up, down, left, right. These properties
+    // are instances of Phaser.Key
     //
     // All we need to do after using this, is poll these in our update loop
     cursors = game.input.keyboard.createCursorKeys();
@@ -130,6 +133,7 @@ function update() {
     game.physics.arcade.collide(stars, platforms);
     game.physics.arcade.collide(baddie, platforms);
     game.physics.arcade.collide(player, baddie, checkLives);
+    game.physics.arcade.collide(diamonds, platforms);
 
     // Check for overlap between player and any star in the stars group
     // If any overlap is detected, pass them to a callback function
@@ -173,6 +177,14 @@ function update() {
       baddie.animations.play('right');
     }
 
+    if(Math.random() < .0005 ) {
+
+      var diamond = diamonds.create((Math.random() * 700) + 50, 0, 'diamond');
+      diamond.body.gravity.y = 12;
+      diamond.body.collideWorldBounds = true;
+
+    }
+
 }
 
 function launchBaddie() {
@@ -202,7 +214,7 @@ function collectStar(player, star) {
 
 }
 
-function checkLives(player, baddie) {
+function checkLives(player) {
   if(playerLives > 1) {
     ughSound.play();
     playerLives--;
